853c9bdfb590c450438f7347cd4c95280231ea9e
Andrey Ryzhov <dune@rznlf.ru>
2018-04-26 17:07:12 +0300

    Add blank first class.

---
+++ src/SimpleReplace.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * ----------------------------------------------
+ * | Author: Andrey Ryzhov (Dune) <info@rznw.ru> |
+ * | Site: www.rznw.ru                           |
+ * | Phone: +7 (4912) 51-10-23                   |
+ * | Date: 26.04.2018                            |
+ * -----------------------------------------------
+ *
+ */
+
+
+namespace AndyDune\StringReplace;
+
+
+class SimpleReplace
+{
+
+}
